6f32640930d4ed487195028ac9f9908208453584
Matthew Paletta <mattpaletta@gmail.com>
2018-03-25 18:57:32 -0700

    Added support for python3 and test script

---
+++ examples/run_example.sh
@@ -0,0 +1,14 @@
+pwd
+
+python server.py
+
+TEST_EXIT_CODE=`python client.py`
+wait $TEST_EXIT_CODE
+
+if [ -z ${TEST_EXIT_CODE+x} ] || [ "$TEST_EXIT_CODE" -ne 0 ] ; then
+  printf "${RED}Tests Failed${NC} - Exit Code: $TEST_EXIT_CODE\n"
+else
+  printf "${GREEN}Tests Passed${NC}\n"
+fi
+
+exit $TEST_EXIT_CODE
